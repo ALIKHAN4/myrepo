2a5f13ed7a3f9f3b6c9856246179f6022a5f6267
ALIKHAN4 <alikhan680880@gmail.com>
2025-09-26 18:19:40 +0500

    Company id on product category

--- sitech_customizations/models/__init__.py
+++ sitech_customizations/models/__init__.py
@@ -1,11 +1,11 @@
 from . import crm_lead
 from . import crm_lead_line
-
 from . import crm_lead_segment
 from . import crm_lead_type
 from . import crm_lead_subsegment
-
 from . import sales_target
-
 from . import sale_order
 from . import crm_lost_reason
+from . import product_category
+
+
