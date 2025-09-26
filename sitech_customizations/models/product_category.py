e7da944c48734d3b814de253aa09ec1068401fb4
ALIKHAN4 <alikhan680880@gmail.com>
2025-09-26 18:43:06 +0500

    code product category

--- sitech_customizations/models/product_category.py
+++ sitech_customizations/models/product_category.py
@@ -4,3 +4,4 @@ class ProductCategory(models.Model):
     _inherit = 'product.category'
     
     company_id = fields.Many2one('res.company', string='Company')
+    code = fields.Char(string='Code')
